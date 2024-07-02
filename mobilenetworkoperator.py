becf6120f3839155891cc679fe7338cdd8cf5696
jakkpotts <jakkpotts@creamistry.xyz>
2024-07-02 16:39:05 -0700

    updated flask response handling

--- mobilenetworkoperator.py
+++ mobilenetworkoperator.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from twilio.rest import Client
 
 app = Flask(__name__)
@@ -14,9 +14,9 @@ client = Client(account_sid, auth_token)
 def lookup(number):
     try:
         # Fetch carrier and caller-name information
-        lookup = client.lookups.phone_numbers(number).fetch(type=['carrier', 'caller-name'])
-        carrier_info = lookup.carrier or {}
-        caller_name_info = lookup.caller_name or {}
+        lookup_number = client.lookups.v1.phone_numbers(number).fetch(type=['carrier', 'caller-name'])
+        carrier_info = lookup_number.carrier or {}
+        caller_name_info = lookup_number.caller_name or {}
 
         # Render the template with the fetched information
         return render_template('lookup.html',
